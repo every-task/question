b61025d819c8d0fd19074c22a022324d679fb566
xodndlee <dnd5436@gmail.com>
2023-10-24 15:09:52 +0900

    feat: 댓글 service 생성 #9

---
+++ src/main/java/com/playdata/domain/comment/service/CommentService.java
@@ -0,0 +1,9 @@
+package com.playdata.domain.comment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+}
