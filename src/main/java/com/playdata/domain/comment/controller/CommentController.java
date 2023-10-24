903d12b739aef86c8a0bc58765d4f3a90f938f2b
xodndlee <dnd5436@gmail.com>
2023-10-24 15:10:00 +0900

    feat: 댓글 controller 생성 #9

---
+++ src/main/java/com/playdata/domain/comment/controller/CommentController.java
@@ -0,0 +1,11 @@
+package com.playdata.domain.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/question/comment")
+@RequiredArgsConstructor
+public class CommentController {
+}
