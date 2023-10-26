04219d6ed51e9913d8274a3f92644f8b3893437d
xodndlee <dnd5436@gmail.com>
2023-10-26 09:41:46 +0900

    refactor: CommentControoler @PostMapping 빠진 실수 수정 #11

--- src/main/java/com/playdata/domain/comment/controller/CommentController.java
+++ src/main/java/com/playdata/domain/comment/controller/CommentController.java
@@ -3,18 +3,18 @@ package com.playdata.domain.comment.controller;
 import com.playdata.domain.comment.request.CommentRequest;
 import com.playdata.domain.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/question/comment")
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+    @PostMapping
     public void insertComment(@RequestBody CommentRequest commentRequest)
     {
-
+        commentService.insertComment(commentRequest);
     }
+
 
 }
