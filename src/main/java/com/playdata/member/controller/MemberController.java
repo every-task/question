53295ef7102c9a0ae9146beb9f639d493645c340
Amy <wlgpek90@naver.com>
2023-10-26 17:21:18 +0900

    feat : 카프카 발행과 구독 위해 필요한 로직, 필드 구현 #6

---
+++ src/main/java/com/playdata/member/controller/MemberController.java
@@ -0,0 +1,21 @@
+package com.playdata.member.controller;
+
+import com.playdata.domain.member.request.MemberRequest;
+import com.playdata.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/question")
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping("/member")
+    public void memberSave(@RequestBody MemberRequest memberRequest) {
+        memberService.memberSave(memberRequest);
+    }
+}
