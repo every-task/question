07d992412f2ff84a8db7b9005458b00c50e810c9
xodndlee <dnd5436@gmail.com>
2023-10-26 18:02:06 +0900

    feat: Member Entity all builder 사용을 위해 @builder, @AllArgs 추가 #6

--- src/main/java/com/playdata/domain/member/entity/Member.java
+++ src/main/java/com/playdata/domain/member/entity/Member.java
@@ -2,9 +2,7 @@ package com.playdata.domain.member.entity;
 
 import com.playdata.domain.article.entity.Article;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,6 +11,9 @@ import java.util.UUID;
 @Table(name = "member")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+//builder all args 추가함
+@Builder
+@AllArgsConstructor
 public class Member {
     @Id
     private UUID id;
@@ -23,4 +24,9 @@ public class Member {
     @OneToMany(mappedBy = "member")
     private List<Article> articles;
 
+    public Member(UUID id, String nickname, String profileImageUrl) {
+        this.id=id;
+        this.nickname=nickname;
+        this.profileImageUrl=profileImageUrl;
+    }
 }
