b958468708939a35beab9c43c159fa85c26b2dec
xodndlee <dnd5436@gmail.com>
2023-10-26 09:38:20 +0900

    feat: ArticleResponse CommentList 필드 추가 #11

--- src/main/java/com/playdata/domain/article/response/ArticleResponse.java
+++ src/main/java/com/playdata/domain/article/response/ArticleResponse.java
@@ -2,11 +2,15 @@ package com.playdata.domain.article.response;
 
 import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.article.request.ArticleRequest;
+import com.playdata.domain.comment.entity.Comment;
+import com.playdata.domain.comment.response.CommentResponse;
 import com.playdata.domain.member.entity.Member;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,9 +19,17 @@ public class ArticleResponse {
     private String title;
     private String content;
     private Member member;
+    private List<Comment> commentList;
 
     @QueryProjection
     public ArticleResponse(Article article)
+    {
+        this.title=article.getTitle();
+        this.content=article.getContent();
+        this.member= article.getMember();
+    }
+    @QueryProjection
+    public ArticleResponse(Article article, List<Comment> commentList)
     {
         this.title=article.getTitle();
         this.content=article.getContent();
