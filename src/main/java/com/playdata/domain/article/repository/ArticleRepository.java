9cd2096a9446ae154dfeadc2ecd0945bc08754d8
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-10-30 11:03:11 +0900

    refactor: ArticleRepository ArticleQueryDslRepository 추가 #24

--- src/main/java/com/playdata/domain/article/repository/ArticleRepository.java
+++ src/main/java/com/playdata/domain/article/repository/ArticleRepository.java
@@ -3,7 +3,7 @@ package com.playdata.domain.article.repository;
 import com.playdata.domain.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends JpaRepository<Article,Long>,ArticleQueryDslRepository {
 
 
 //    @Query("select a from Article as a where a.id = :id")
