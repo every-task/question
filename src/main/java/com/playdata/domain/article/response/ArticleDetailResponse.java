3603cadddf8a05607c414574e99d6aa824039f66
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-10-31 21:39:55 +0900

    feat: ArticleDetailReponse파일 추가  #32

---
+++ src/main/java/com/playdata/domain/article/response/ArticleDetailResponse.java
@@ -0,0 +1,32 @@
+package com.playdata.domain.article.response;
+
+import com.playdata.domain.article.dto.ArticleDto;
+import com.playdata.domain.article.entity.Article;
+import com.playdata.domain.comment.dto.CommentDto;
+import com.playdata.domain.comment.response.CommentResponse;
+import com.playdata.domain.member.entity.Member;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Getter
+public class ArticleDetailResponse extends ArticleDto {
+
+    private List<CommentDto> comments;
+    private Member member;
+
+    public ArticleDetailResponse(Article article)
+    {
+        super(article);
+        this.member=article.getMember();
+        this.comments = article.getComments() != null ?
+                article.getComments()
+                        .stream()
+                        .map(CommentDto::new).toList() :new ArrayList<>();
+
+    }
+}
