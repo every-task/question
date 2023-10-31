9287af4cdc04e069dd67c9547834337b6eb74728
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-10-31 21:39:13 +0900

    refactor: CommentResponse 주석지움  #32

--- src/main/java/com/playdata/domain/comment/response/CommentResponse.java
+++ src/main/java/com/playdata/domain/comment/response/CommentResponse.java
@@ -3,22 +3,20 @@ package com.playdata.domain.comment.response;
 import com.playdata.domain.article.dto.ArticleDto;
 import com.playdata.domain.comment.dto.CommentDto;
 import com.playdata.domain.comment.entity.Comment;
+import com.playdata.domain.member.entity.Member;
 import lombok.Getter;
 
 
 @Getter
 public class CommentResponse extends CommentDto {
-    //    private Long id;
-//    private String content;
-//    private Member member;
-    private ArticleDto article;
+    private Member member;
+
+
+
 
     public CommentResponse(Comment comment) {
         super(comment);
-        article = new ArticleDto(comment.getArticle());
-//        this.id = comment.getId();
-//        this.content = comment.getContent();
-//        this.member = comment.getMember();
+        this.member=comment.getMember();
     }
 
 
