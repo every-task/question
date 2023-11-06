ab6c65cee7b7ffe8ca4369307925d2ae4d291b86
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-11-06 17:40:54 +0900

    refactor:예외처리 e.cause형태에서 영어로 log 형태로 변경 #45

--- src/main/java/com/playdata/exception/CustomExceptionAdvice.java
+++ src/main/java/com/playdata/exception/CustomExceptionAdvice.java
@@ -4,24 +4,25 @@ package com.playdata.exception;
 import com.playdata.exception.response.ExceptionResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.WeakKeyException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class CustomExceptionAdvice {
 
     @ExceptionHandler(NoArticleByIdException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String resourceNotFoundExceptionHandler(NoArticleByIdException e) {
         return e.getMessage();
     }
     @ExceptionHandler(ExpiredJwtException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ExceptionResponse weakKeyExceptionHandler(ExpiredJwtException e){
-        return new ExceptionResponse(
-                "토큰의 사용기간이 지났습니다.", e.getCause());
+    public void weakKeyExceptionHandler(ExpiredJwtException e){
+        log.error("error 401", e);
     }
 
 
