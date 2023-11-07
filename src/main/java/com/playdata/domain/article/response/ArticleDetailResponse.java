cfd249487f3b0807362eb7d89628a3d7f0a47f01
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-11-07 16:48:47 +0900

    refactor: 주석 지워줌#47

--- src/main/java/com/playdata/domain/article/response/ArticleDetailResponse.java
+++ src/main/java/com/playdata/domain/article/response/ArticleDetailResponse.java
@@ -3,18 +3,13 @@ package com.playdata.domain.article.response;
 import com.playdata.domain.article.dto.ArticleDto;
 import com.playdata.domain.article.entity.Article;
 import com.playdata.domain.comment.dto.CommentDto;
-import com.playdata.domain.comment.response.CommentResponse;
 import com.playdata.domain.member.dto.MemberDto;
-import com.playdata.domain.member.entity.Member;
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-//@AllArgsConstructor
-//@NoArgsConstructor
+
 @Getter
 public class ArticleDetailResponse extends ArticleDto {
 
