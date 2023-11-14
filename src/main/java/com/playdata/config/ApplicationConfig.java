870f8b6d6b2386b742f915298ddc59137ca53e01
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-11-14 15:57:23 +0900

    feat:파일 수정#dev

--- src/main/java/com/playdata/config/ApplicationConfig.java
+++ src/main/java/com/playdata/config/ApplicationConfig.java
@@ -13,21 +13,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class ApplicationConfig {
     private final JwtService jwtService;
-
-    @Value("${config.password.strength}")
-    private int STRENGTH;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(STRENGTH);
-    }
-
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authenticationProvider
                 = new DaoAuthenticationProvider();
-
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
         authenticationProvider.setUserDetailsService(jwtService::parseToken);
 
         return authenticationProvider;
