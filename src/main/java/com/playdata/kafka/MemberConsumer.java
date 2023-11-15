565149f6bd63b5415b8b985be96a71c862b48dbd
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-11-15 16:19:08 +0900

    refactor:member kafka 데이터 enum값의 따라 다르게 처리#63

--- src/main/java/com/playdata/kafka/MemberConsumer.java
+++ src/main/java/com/playdata/kafka/MemberConsumer.java
@@ -1,28 +1,40 @@
 package com.playdata.kafka;
 
 import com.playdata.domain.member.entity.Member;
-import com.playdata.domain.member.kafka.MemberKafkaData;
+import com.playdata.domain.member.kafka.MemberKafka;
 import com.playdata.domain.member.repository.MemberRepository;
+import com.playdata.exception.NoArticleByIdException;
+import com.playdata.exception.NoMemberByIdException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.ToDoubleBiFunction;
 
 @Component
 @RequiredArgsConstructor
 public class MemberConsumer {
     private final MemberRepository memberRepository;
 
+    // TODO memberListen을 할 때 각각의 action의 따라서 다른 결과가 되어야 함
+    // TODO 현재는 받아서 로직으로 구현 insert, update, delete를 action에서 확인해서 처리
     @KafkaListener(topics = TopicConfig.MEMBER)
-    public void listen(MemberKafkaData data) {
-        Optional<Member> member =memberRepository.findById(UUID.fromString(data.id()));
-        if(member.isEmpty()) memberRepository.save(data.ToEntity());
-        else{
-            Member member1 = member.get();
-            member1.setNickname(data.nickname());
-            member1.setProfileImageUrl(data.profileImageUrl());
+    public void listen(MemberKafka data) {
+        if(data.action().toString().equals("CREATE"))
+        {
+            memberRepository.save(data.ToEntity());
+        }
+        else if(data.action().toString().equals("UPDATE"))
+        {
+            memberRepository.save(data.ToEntity());
+        }
+        else {
+            Optional<Member> getMemberByKafkaData = memberRepository.findById(UUID.fromString(data.id()));
+            Member member = getMemberByKafkaData.orElseThrow(()->
+                    new NoMemberByIdException("No Member . id = {%s}".formatted(data.id())));
+            member.delete();
         }
     }
     @KafkaListener(topics = TopicConfig.memberDLT)
