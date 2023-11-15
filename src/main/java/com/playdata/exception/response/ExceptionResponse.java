cabf8cf34cc9f71d00016863c4b487a16222d6e5
taewoonglee <102516228+taewoonglee@users.noreply.github.com>
2023-11-15 16:18:22 +0900

    refactor:겹치는 status code 지움#63

--- src/main/java/com/playdata/exception/response/ExceptionResponse.java
+++ src/main/java/com/playdata/exception/response/ExceptionResponse.java
@@ -10,13 +10,11 @@ import static com.playdata.exception.status.Status.FAILED;
 @Getter
 public class ExceptionResponse {
     private String status;
-    private String code;
     private String message;
-    public static ExceptionResponse responseBuilder(String code, String message) {
+    public static ExceptionResponse responseBuilder(String message) {
         return ExceptionResponse
                 .builder()
                 .status(FAILED.toString())
-                .code(code)
                 .message(message)
                 .build();
     }
